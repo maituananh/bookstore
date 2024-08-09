8e033cb3ced77ebcd8b2b2bfe4c5d983ab76bdf4
AnhMaiTuan <amaituan@openwt.com>
2024-08-09 17:52:53 +0700

    training

--- app/src/main/java/com/example/bookstore/action/IRecyclerAction.kt
+++ app/src/main/java/com/example/bookstore/action/IRecyclerAction.kt
@@ -1,9 +1,8 @@
 package com.example.bookstore.action
 
-import com.example.domain.model.book.Book
+import com.example.bookstore.adapter.RecyclerAdapter
 
 interface IRecyclerAction<T> {
-    fun onClick(position: Int)
-
     fun onClick(book: T)
+    fun bindingDataToItemLayout(holder: RecyclerAdapter<T>.ViewHolder, position: Int, data: List<T>)
 }
---
+++ domain/src/main/java/com/example/domain/model/calendar/Calendar.kt
@@ -0,0 +1,4 @@
+package com.example.domain.model.calendar
+
+class Calendar {
+}
